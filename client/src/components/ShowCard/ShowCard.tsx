1bbbb0f4b0bb5a0f28979714d3cc2fa2d238178b
Michael Carlin <michaelcarlin076@gmail.com>
2024-09-30 11:52:10 +0100

    TailwindCSS styling completed. SetInterval to retrieve data from endpoint every 5 minutes to keep it up-to-date with CRON requests.

--- client/src/components/ShowCard/ShowCard.tsx
+++ client/src/components/ShowCard/ShowCard.tsx
@@ -1,33 +1,57 @@
 import { ShowData } from "../../types";
 
 export default function ShowCard(showInfo: ShowData) {
+  const isSoldOut = showInfo.bookingLink === "SOLD OUT";
+
   return (
     <div
-      className={`border rounded-lg p-2 shadow-lg flex flex-col justify-between ${
-        showInfo.bookingLink === "SOLD OUT" ? "bg-gray-300" : "bg-white"
-      } h-[305px] w-[350px]`} // Updated height and width
+      className={`border rounded-lg shadow-lg flex flex-col justify-between ${
+        isSoldOut ? "bg-gray-300" : "bg-white"
+      } h-[350px] w-[350px]`}
     >
-      <img
-        src={showInfo.image}
-        alt={showInfo.name}
-        className="w-full h-[70%] object-cover rounded-md" // Full width, height is 70% of card
-      />
-      <h2 className="text-sm font-semibold mb-1 text-center">
-        {showInfo.name}
-      </h2>
-      <h3 className="text-xs font-medium mb-1 text-center">About the Show</h3>
-      {showInfo.bookingLink !== "SOLD OUT" ? (
-        <a
-          href={`${showInfo.bookingLink}`}
-          className="bg-blue-500 text-white text-center py-1 px-2 rounded-md hover:bg-blue-600 transition duration-300 text-xs"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex-1">
+        <img
+          src={showInfo.image}
+          alt={showInfo.name}
+          className={`w-full h-full object-cover rounded-t-md ${
+            isSoldOut ? "opacity-25" : ""
+          }`}
+        />
+      </div>
+
+      <div className="p-3">
+        <h1
+          className={`text-xl font-bold mb-1 text-center ${
+            isSoldOut ? "text-gray-500 opacity-50" : ""
+          }`}
         >
-          Buy Now
-        </a>
-      ) : (
-        <h3 className="text-center font-bold text-xs">SOLD OUT</h3>
-      )}
+          {showInfo.name}
+        </h1>
+        <h3
+          className={`text-md font-medium mb-1 text-center ${
+            isSoldOut ? "text-gray-500 opacity-50" : ""
+          }`}
+        >
+          About the Show
+        </h3>
+      </div>
+
+      <div className="">
+        {isSoldOut ? (
+          <h3 className="text-center font-bold text-md pb-2 text-gray-500 opacity-50">
+            SOLD OUT
+          </h3>
+        ) : (
+          <a
+            href={`${showInfo.bookingLink}`}
+            className="bg-gradient-to-r from-orange-500 to-red-500 text-white text-center py-2 rounded-b-md hover:from-red-500 hover:to-orange-500 transition duration-300 text-md h-full w-full flex items-center justify-center"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Book Now
+          </a>
+        )}
+      </div>
     </div>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -20,12 +20,18 @@ export default function Home() {
 
   useEffect(() => {
     getShowData();
+
+    const intervalId = setInterval(() => {
+      getShowData();
+    }, 5 * 60 * 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4 text-center">Today's Deals</h1>
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
+      <h1 className="text-3xl font-bold mb-4">Today's Deals</h1>
+      <div className="grid gap-12 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center">
         {showData
           ? showData.map((showInfo: ShowData, index: number) => {
               return <ShowCard {...showInfo} key={index} />;
